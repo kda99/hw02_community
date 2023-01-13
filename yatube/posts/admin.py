777d3fe384b604075f8a33a2fdc8a643acaf1050
Дмитрий Капустин <kda99@mail.ru>
2023-01-14 00:39:08 +0300

    проверил flake8

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
 from .models import Post
 from .models import Group
+
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
@@ -14,11 +14,6 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('pub_date', )
     # Замена пустого поля
     empty_value_display = '-пусто-'
-
-# При регистрации модели Post источником конфигурации для неё назначаем
-# класс PostAdmin
-
-
 
 
 admin.site.register(Post, PostAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,6 @@ from .models import Post, Group
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
     context = {
@@ -24,4 +21,3 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
