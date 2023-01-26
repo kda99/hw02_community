8fd8483c1b6aedab34ac0f10266eaab077573bc1
Дмитрий Капустин <kda99@mail.ru>
2023-01-26 20:12:42 +0300

    Спринт 4/14 → Тема 4/6: Дополнительные компоненты Django Paginator group_list.html

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -26,9 +26,13 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    post_list = Post.objects.filter(group=group).order_by('-pub_date')
+    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
